76690d1c3faa88fa2aec1832233b0abc64d7e990
TTTin239 <trantrungtin2309@gmail.com>
2022-12-26 00:48:23 +0700

    more review list

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer/Footer';
 import About from './pages/About/About';
 import ListProduct from './pages/ListProduct';
 import HomePage from './pages/HomePage';
+import Reviews from './pages/Reviews/index';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path='/about' element={<About />} />
           <Route path='/products' element={<ListProduct />} />
+          <Route path='/reviews' element={<Reviews />} />
         </Routes>
         <Footer/>
       </Router>
